fc2fc8d56461dee9d304d6e7580b276e51f42f39
sandeep <sandeepupmp@gmail.com>
2024-04-28 10:47:18 +0530

    route added and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
-
-
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
- 
-
   return (
-    <>
-     <h1>heloo vite</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/profile" element = {<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
